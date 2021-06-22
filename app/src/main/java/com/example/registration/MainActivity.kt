dabfefbe42ebbf394c5d3cca9e14079b025d07dd
Okola44 <mercyimaliokola@gmail.com>
2021-06-22 06:23:12 +0300

    register

--- app/src/main/java/com/example/registration/MainActivity.kt
+++ app/src/main/java/com/example/registration/MainActivity.kt
@@ -3,17 +3,15 @@ package com.example.registration
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var etName:EditText
-    lateinit var etDate:EditText
+    lateinit var etDob:EditText
     lateinit var etIdnum:EditText
-    lateinit var etNationality:EditText
-    lateinit var etPhone:EditText
+    lateinit var spNationality:Spinner
+    lateinit var etPhoneNum:EditText
     lateinit var etEmail:EditText
     lateinit var btlbl:Button
 
@@ -30,12 +28,20 @@ class MainActivity : AppCompatActivity() {
 
     fun castViews(){
          etName = findViewById(R.id.etName)//R.ID IS A MAPPING BTWN THE VIEW AND THE INTEGER
-         etDate = findViewById(R.id.etDate)
-         etIdnum = findViewById(R.id.etIdnum)
-         etNationality = findViewById(R.id.etNationality)
-         etPhone = findViewById(R.id.etPhone)
-        etEmail = findViewById(R.id.etEmail)
-        btlbl = findViewById(R.id.btlbl)
+         etDob = findViewById(R.id.etDob)
+         etIdnum = findViewById(R.id.etIdNum)
+
+         etPhoneNum = findViewById(R.id.etPhoneNum)
+         etEmail = findViewById(R.id.etEmail)
+         btlbl = findViewById(R.id.btlbl)
+        //code for  the spinner
+        //select nationality is the default when you run your code
+        spNationality=findViewById(R.id.spNationality)
+        var nationalities= arrayOf("select Nationality","Kenyan","Rwandan","South Sudanese","Ugandan")
+        var nationalityAdapter=ArrayAdapter(baseContext,android.R.layout.simple_spinner_item,nationalities)
+        nationalityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spNationality.adapter=nationalityAdapter
+
 
     }
     fun clickRegister() {
@@ -45,11 +51,12 @@ class MainActivity : AppCompatActivity() {
             if (name.isEmpty()) {
                 etName.setError("THIS FIELD IS REQUIRED")//THROWS ERROR IF NAME IS NOT FILLED
             }
-            var dob = etDate.text.toString()
+            var dob = etDob.text.toString()
             var email = etEmail.text.toString()
             var id = etIdnum.text.toString()
-            var nationality = etNationality.text.toString()
-            var phone = etPhone.text.toString()
+            var nationality = spNationality.selectedItem.toString()
+
+            var phone = etPhoneNum.text.toString()
             var Student = Student(name, dob, id, nationality, phone, email)
             Toast.makeText(baseContext, Student.toString(), Toast.LENGTH_LONG).show()
             val intent=Intent(baseContext,coursesActivity::class.java)
@@ -58,4 +65,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+//spinner set error,for now we can use a toast
+//you can add a set error and error message
+//android spinner change drop down icon
+
 data class Student(var name:String,var dob:String,var id:String,var nationality:String,var phone:String,var email:String)//STORES EVERYTHING
--- app/src/main/java/com/example/registration/coursesActivity.kt
+++ app/src/main/java/com/example/registration/coursesActivity.kt
@@ -6,14 +6,22 @@ import android.os.Bundle
 import android.widget.Button
 
 class coursesActivity : AppCompatActivity() {
+    lateinit var btngo:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_courses)
+        castViews2()
+        clickRegister2()
+        }
+        fun castViews2(){
+             btngo=findViewById(R.id.btngo)
 
-        var btngo=findViewById<Button>(R.id.btngo)
-        btngo.setOnClickListener {
-            var intent=Intent(baseContext,loginActivity::class.java)
-            startActivity(intent)
+        }
+        fun clickRegister2(){
+            btngo.setOnClickListener {
+                var intent=Intent(baseContext,loginActivity::class.java)
+                startActivity(intent)
+
         }
     }
 }
